b36e88b214516773e4c170140f6a58b5cbf83970
Vladislav Neretin <rinosetou@gmail.com>
2022-05-13 10:56:31 +0300

    v.1

---
+++ hw1.2/index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+require('dotenv').config();
+const getCurrentDate = require('./time');
+
+let id = 0;
+
+http
+  .createServer(function (res, req) {
+    if (res.url !== '/') {
+      return false;
+    }
+
+    id++;
+    const intervalId = setInterval(() => {
+      console.log(id, getCurrentDate());
+    }, process.env.INTERVAL_TIME);
+
+    setTimeout(() => {
+      clearInterval(intervalId);
+      req.end(getCurrentDate());
+    }, process.env.STOP_TIMER_TIME);
+  })
+  .listen(process.env.SERVER_PORT);
+
+console.log(`HTTP server running on port ${process.env.SERVER_PORT}`);
